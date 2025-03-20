3e9a7a2bf720ff39d569c36693b7555d5bac4b1b
Gideon Mrina <gidepan002@gmail.com>
2025-03-21 01:42:53 +0300

    changing port

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const router = require('./routes');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 app.use(cors());
